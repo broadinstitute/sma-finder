b963257fa8f41e3cc0bf98ae560aa7cd761506ba
bw2 <ben.weisburd@gmail.com>
2023-08-07 20:45:09 -0400

    refactored plot and improved error checking

--- plot_SMN1_SMN2_scatter.py
+++ plot_SMN1_SMN2_scatter.py
@@ -1,11 +1,11 @@
-
-#%%
 import argparse
 import json
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pandas as pd
+import re
 import seaborn as sns
 import string
 
@@ -19,8 +19,7 @@ PHRED_SCALE_CONFIDENCE_THRESHOLD = 10
 
 UNKNOWN_SAMPLES_LABEL = "Rare Disease Cases"
 NEGATIVE_CONTROL_LABEL = "Unaffected Relatives"
-POSITIVE_CONTROL_LABEL = "Cases With Confirmed\n" \
-                         "SMA Diagnosis"
+POSITIVE_CONTROL_LABEL = "Cases With Validated\nSMA Diagnosis"
 
 PALETTE = {
     UNKNOWN_SAMPLES_LABEL: "tab:gray",
@@ -28,6 +27,10 @@ PALETTE = {
     POSITIVE_CONTROL_LABEL: "tab:red",
 }
 
+
+C840_SMN1_READS_COLUMN = "c840_reads_with_smn1_base_C"
+C840_TOTAL_READS_COLUMN = "c840_total_reads"
+SMA_STATUS_COLUMN = "sma_status"
 
 def compute_decision_boundary_slope_and_intercept(p, p_error, confidence_threshold):
     """Compute the slope of the decision boundary line when calling SMN1 copy number (0 or greater than 0)
@@ -53,162 +56,179 @@ def compute_decision_boundary_slope_and_intercept(p, p_error, confidence_thresho
     return numerator / denominator, confidence_threshold / denominator
 
 
-def plot_figure(df_all):
-
-    exon_coverage_columns = [c for c in df_all.columns if c.startswith("reads_at_exon")]
-    df_all.loc[:, "Average reads at exons 1-6"] = df_all[exon_coverage_columns].astype(int).sum(axis=1)/len(
-        exon_coverage_columns)
-
-    fig, ax = plt.subplots(1, 2, figsize=(14, 6), constrained_layout=True)
-    plt.subplots_adjust(left=0.05, right=0.85, top=0.95, bottom=0.05, hspace=0.3)
-    fig.set_tight_layout(True)
-    #fig.suptitle('sma_finder results for WGS, WES, and RNA samples')
-
-    df_wgs = df_all[df_all.sample_type == "WGS"]
-    df_wes = df_all[df_all.sample_type == "WES"]
-    n_missing_sample_type = sum(df_all.sample_type.isna())
-    assert len(df_wgs) + len(df_wes) + n_missing_sample_type == len(df_all)
-
-    if n_missing_sample_type > 0:
-        print(f"WARNING: sample type is missing for {n_missing_sample_type} samples")
+def plot_figure(df_all, title=None):
 
     # compute decision boundary x,y points.
-    # Decision boundary 1 = without a confidence threshold (shown as a solid line)
-    # Decision boundary 2 = with a confidence threshold (shown as a dashed line)
-    slope, intercept = compute_decision_boundary_slope_and_intercept(P_SMN1, P_ERROR, 0)
-    slope2, intercept2 = compute_decision_boundary_slope_and_intercept(P_SMN1, P_ERROR, PHRED_SCALE_CONFIDENCE_THRESHOLD)
+    decision_boundary_x = np.linspace(0, 1000, 100000)
 
-    decision_boundary_x_for_all_samples = np.linspace(0, 1000, 100000)
-    decision_boundary_y = slope * decision_boundary_x_for_all_samples + intercept
+    # decision boundary 1 = without a confidence threshold (shown as a solid line)
+    slope, intercept = compute_decision_boundary_slope_and_intercept(P_SMN1, P_ERROR, 0)
+    decision_boundary_y = slope * decision_boundary_x + intercept
     decision_boundary_y[decision_boundary_y < MIN_COVERAGE_THRESHOLD - 0.5] = MIN_COVERAGE_THRESHOLD - 0.5
 
-    decision_boundary2_y = slope2 * decision_boundary_x_for_all_samples + intercept2
-    decision_boundary2_y[decision_boundary2_y < MIN_COVERAGE_THRESHOLD - 0.5] = MIN_COVERAGE_THRESHOLD - 0.5
+    # decision boundary 2 = with a confidence threshold (shown as a dashed line)
+    #slope2, intercept2 = compute_decision_boundary_slope_and_intercept(P_SMN1, P_ERROR, PHRED_SCALE_CONFIDENCE_THRESHOLD)
+    #decision_boundary2_y = slope2 * decision_boundary_x + intercept2
+    #decision_boundary2_y[decision_boundary2_y < MIN_COVERAGE_THRESHOLD - 0.5] = MIN_COVERAGE_THRESHOLD - 0.5
 
-    # generate the 4 panels of Figure 3
+    # initialize the figure
+    fig, ax = plt.subplots(1, 1, figsize=(9.2, 6.5))
+    plt.subplots_adjust(left=0.05, right=0.85, top=0.95, bottom=0.05, hspace=0.3)
+    fig.set_tight_layout(True)
+    if title: fig.suptitle(title, fontsize=17)
+
     x_axis_label = "# of reads that contain a 'C' at the c.840 position"
-    y_axis_label, total_reads_column = "Total # of reads at the c.840 position", "c840_total_reads"
-    decision_boundary_x = decision_boundary_x_for_all_samples
+    y_axis_label = "Total # of reads at the c.840 position"
 
-    for ax_j, (df_for_sample_type, sample_type) in enumerate([(df_wgs, "WGS"), (df_wes, "WES")]):
+    # add jitter to points below the minimum coverage threshold to make them visible
+    jitter = (np.random.rand(len(df_all)) - 0.5)/4
+    df_all.loc[
+        df_all[C840_TOTAL_READS_COLUMN] < MIN_COVERAGE_THRESHOLD,
+        C840_TOTAL_READS_COLUMN
+    ] = df_all[C840_TOTAL_READS_COLUMN] + jitter
 
-        # add jitter to points below the minimum coverage threshold
-        jitter = (np.random.rand(len(df_for_sample_type)) - 0.5)/4
-        df_for_sample_type.loc[
-            df_for_sample_type["c840_total_reads"] < MIN_COVERAGE_THRESHOLD,
-            total_reads_column
-        ] = df_for_sample_type[total_reads_column] + jitter
+    # render data points
+    sns.scatterplot(
+        ax=ax,
+        data=df_all,
+        x=C840_SMN1_READS_COLUMN,
+        y=C840_TOTAL_READS_COLUMN,
+        hue=SMA_STATUS_COLUMN,
+        palette=PALETTE,
+        s=15)
 
-        current_ax = ax[ax_j]
+    # text labels for points that should be labeled
+    df_samples_to_label = df_all[df_all["show_label"]]
 
-        # render data points
-        sns.scatterplot(
-            ax=current_ax,
-            data=df_for_sample_type,
-            x="c840_reads_with_smn1_base_C",
-            y=total_reads_column,
-            hue="sma_status",
-            palette=PALETTE,
-            s=15)
+    if len(df_samples_to_label) > 0:
+        for _, row in df_samples_to_label.iterrows():
+            x_offset = 2
+            y_offset = 50
+            ax.text(
+                row[C840_SMN1_READS_COLUMN] + x_offset, row[C840_TOTAL_READS_COLUMN] + y_offset,
+                row["sample_id"], size=12)
+            ax.arrow(
+                row[C840_SMN1_READS_COLUMN] + x_offset - 0.5, row[C840_TOTAL_READS_COLUMN] + y_offset - 0.5,
+                -x_offset + 0.5 + 0.3, -y_offset + 0.5 + 2,
+                head_width=x_offset*0.1, head_length=y_offset*0.15)
 
-        # add text label for new points
-        df_samples_to_label = df_for_sample_type[df_for_sample_type["show_label"]]
+    ax.set_xlim([-0.5, 1000])
+    ax.set_ylim([-0.5, 1000])
+    ax.set_xscale("symlog", linthresh=MIN_COVERAGE_THRESHOLD)
+    ax.set_yscale("symlog", linthresh=MIN_COVERAGE_THRESHOLD)
+    ax.tick_params(labelsize=14)
+    ax.set_xlabel(x_axis_label, size=15, labelpad=15)
+    ax.set_ylabel(y_axis_label, size=15, labelpad=15)
 
-        if len(df_samples_to_label) > 0:
-            for _, row in df_samples_to_label.iterrows():
-                x_offset = 2
-                y_offset = 50
-                current_ax.text(
-                    row["c840_reads_with_smn1_base_C"] + x_offset, row[total_reads_column] + y_offset,
-                    row["sample_id"], size=12)
-                current_ax.arrow(
-                    row["c840_reads_with_smn1_base_C"] + x_offset - 0.5, row[total_reads_column] + y_offset - 0.5,
-                    -x_offset + 0.5 + 0.3, -y_offset + 0.5 + 2,
-                    head_width=x_offset*0.1, head_length=y_offset*0.15)
+    # decision boundary line(s)
+    decision_boundary_line_color = "red"
+    ax.plot(
+        decision_boundary_x,
+        decision_boundary_y,
+        '-',
+        label='Decision Boundary',
+        c=decision_boundary_line_color)
 
-        current_ax.set_xlim([-0.5, 1000])
-        current_ax.set_ylim([-0.5, 1000])
-        current_ax.set_xscale("symlog", linthresh=MIN_COVERAGE_THRESHOLD)
-        current_ax.set_yscale("symlog", linthresh=MIN_COVERAGE_THRESHOLD)
-        current_ax.tick_params(labelsize=14)
-        current_ax.set_xlabel(x_axis_label, size=15, labelpad=15)
+    # minimum coverage threshold line
+    ax.plot(
+        [0, MIN_COVERAGE_THRESHOLD-0.5],
+        [MIN_COVERAGE_THRESHOLD-0.5, MIN_COVERAGE_THRESHOLD-0.5],
+        linestyle=(0, (5, 1)),
+        linewidth=0.8,
+        color='black',
+        label="Minimum Read\nCoverage Threshold")
 
-        current_ax.set_ylabel(y_axis_label if ax_j == 0 else "", size=15, labelpad=15)
-
-
-        # A, B labels above each panel
-        panel_title = f"All {sample_type} samples"
-        current_ax.text(-0.1, 1.07, string.ascii_uppercase[ax_j], transform=current_ax.transAxes, size=20, weight='bold')
-        current_ax.text(-0.01, 1.07, panel_title, transform=current_ax.transAxes, size=18)
-
-        # decision boundary line (s)
-        decision_boundary_line_color = "red"
-        current_ax.plot(
-            decision_boundary_x,
-            decision_boundary_y,
-            '-',
-            label='Decision Boundary',
-            c=decision_boundary_line_color)
-
-        # minimum coverage threshold line
-        current_ax.plot(
-            [0, MIN_COVERAGE_THRESHOLD-0.5],
-            [MIN_COVERAGE_THRESHOLD-0.5, MIN_COVERAGE_THRESHOLD-0.5],
-            linestyle=(0, (5, 1)),
-            linewidth=0.8,
-            color='black',
-            label="Minimum Read\nCoverage Threshold")
-
-        # legend
-        if ax_j > 0:
-            current_ax.legend(
-                loc='upper left',
-                bbox_to_anchor=[1.05, 1],
-                frameon=False)
-        else:
-            current_ax.get_legend().remove()
-
-    #fig.subplots_adjust(right=0.8, left=0.05)  # , top=0.9, bottom=0.1, wspace=0.2
+    # legend
+    ax.legend(
+        loc='upper left',
+        bbox_to_anchor=[1.05, 1],
+        frameon=False)
 
 
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument("--positive-controls-path", default="positive_controls.txt",
-                   help="Path of text file containing a list of positive control sample ids (one per line)")
-    p.add_argument("--no-label-path", default="no_label_samples.txt",
-                   help="Path of text file containing a list of sample ids that shouldn't be labeled on the plot")    
-    p.add_argument("-o", "--output-prefix", default="figure3",
-                   help="Output prefix")
-    p.add_argument("sma_finder_combined_results_path",
-                   help="Path of tsv file containing the combined results of SMA Finder for all WES and WGS samples")
+    p.add_argument("-p", "--positive-control", action="append", help="Sample id of a positive control. "
+                   "This argument can be specified more than once")
+    p.add_argument("-P", "--positive-control-sample-ids-path", help="Path of text file containing a list of positive "
+                   "control sample ids (one per line)")
+    p.add_argument("-l", "--sample-id-to-label", action="append", help="Sample id that should be labeled in the plot. "
+                   "This argument can be specified more than once")
+    p.add_argument("-L", "--sample-ids-to-label-path", help="Path of text file containing a list of sample ids that "
+                   "should be labeled in the plot")
+    p.add_argument("--sample-id-column", default="sample_id", help="Name of the input table column that contains the "
+                   "sample ids")
+    p.add_argument("--affected-status-column", default="affected", help="Name of the input table column that contains "
+                   "the affected status of each sample")
+    p.add_argument("--not-affected-label", default="Not Affected", help="Value in the affected status column that "
+                   "indicates that the sample is unaffected")
+    p.add_argument("--title", help="Plot title")
+    p.add_argument("-o", "--output-prefix", help="Filename prefix for the output plot image file.")
+    p.add_argument("-f", "--output-format", default="png", choices=["png", "pdf", "svg"], help="Output image format")
+    p.add_argument("sma_finder_combined_results_path", help="Path of tsv file containing the combined results of "
+                   "SMA Finder for all samples")
 
-    args, _ = p.parse_known_args()
+    args = p.parse_args()
 
-    positive_controls = set()
-    if args.positive_controls_path:
-        with open(args.positive_controls_path, "rt") as f:
-            positive_controls = {s.strip() for s in f if s.strip()}
-        print(f"Loaded {len(positive_controls)} positive controls: ", positive_controls)
-
-    no_label_sample_ids = set()
-    if args.no_label_path:
-        with open(args.no_label_path, "rt") as f:
-            no_label_sample_ids = {s.strip() for s in f if s.strip()}
-        print(f"Loaded {len(no_label_sample_ids)} no-label sample ids")
+    for path in args.positive_control_sample_ids_path, args.sample_ids_to_label_path, args.sma_finder_combined_results_path:
+        if path and not os.path.isfile(path):
+            p.error(f"File not found: {args.positive_control_sample_ids_path}")
 
     df = pd.read_table(args.sma_finder_combined_results_path)
-    df.loc[:, "sma_status"] = UNKNOWN_SAMPLES_LABEL
-    df.loc[df.affected == "Not Affected", "sma_status"] = NEGATIVE_CONTROL_LABEL
-    df.loc[df["sample_id"].isin(positive_controls), "sma_status"] = POSITIVE_CONTROL_LABEL
-    df["show_label"] = ~df["sample_id"].isin(no_label_sample_ids) & (df["sma_status"] == UNKNOWN_SAMPLES_LABEL) & (
-            df["c840_reads_with_smn1_base_C"] <= MAX_SMN1_READS_THRESHOLD) & (
-            df["c840_total_reads"] >= MIN_COVERAGE_THRESHOLD)
 
-    plot_figure(df)
+    for column_name in args.sample_id_column, args.affected_status_column, C840_SMN1_READS_COLUMN, C840_TOTAL_READS_COLUMN:
+        if column_name not in df.columns:
+            p.error(f"Column '{column_name}' not found in {args.sma_finder_combined_results_path}")
 
-    plt.savefig(f"SMN1_vs_SMN2_plot.png")
+    all_sample_ids = set(df[args.sample_id_column])
 
-    plt.show()
+    positive_controls = set()
+    if args.positive_control_sample_ids_path:
+        with open(args.positive_control_sample_ids_path, "rt") as f:
+            positive_controls |= {s.strip() for s in f if s.strip()}
+
+    sample_ids_to_label = set()
+    if args.sample_ids_to_label_path:
+        with open(args.sample_ids_to_label_path, "rt") as f:
+            sample_ids_to_label |= {s.strip() for s in f if s.strip()}
+
+    for source, sample_ids in [
+        ("--positive-control", args.positive_control),
+        ("--sample-id-to-label", args.sample_id_to_label),
+        (args.positive_control_sample_ids_path, positive_controls),
+        (args.sample_ids_to_label_path, sample_ids_to_label)]:
+        if not sample_ids:
+            continue
+        unknown_sample_ids = set(sample_ids) - all_sample_ids
+        if unknown_sample_ids:
+            print(f"WARNING: Unknown sample id(s) specified in {source}: " + ", ".join(sorted(unknown_sample_ids)))
+
+    if args.positive_control:
+        positive_controls |= set(args.positive_control)
+
+    if args.sample_id_to_label:
+        sample_ids_to_label |= set(args.sample_id_to_label)
+
+    df[SMA_STATUS_COLUMN] = UNKNOWN_SAMPLES_LABEL
+    
+    if args.not_affected_label not in set(df[args.affected_status_column]):
+        print(f"WARNING: --not-affected-label arg value '{args.not_affected_label}' was not found in the "
+              f"{args.affected_status_column} column of {args.sma_finder_combined_results_path}")
+        
+    df.loc[df[args.affected_status_column] == args.not_affected_label, SMA_STATUS_COLUMN] = NEGATIVE_CONTROL_LABEL
+    
+    if positive_controls:
+        df.loc[df["sample_id"].isin(positive_controls), SMA_STATUS_COLUMN] = POSITIVE_CONTROL_LABEL
+    df["show_label"] = df["sample_id"].isin(sample_ids_to_label)
+
+    plot_figure(df, args.title)
+
+    if not args.output_prefix:
+        args.output_prefix = re.sub(".tsv(.gz)?$", "", os.path.basename(args.sma_finder_combined_results_path))
+
+    ouput_path = f"{args.output_prefix}.SMN1_vs_SMN2_plot.{args.output_format}"
+    plt.savefig(ouput_path)
+    print(f"Saved plot to {ouput_path}")
+
     plt.close()
 
 
