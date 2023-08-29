4c6a6d02dd16b487a304661c32586bf14f17c968
bw2 <ben.weisburd@gmail.com>
2023-08-29 09:55:08 -0400

    added --exclude-sample-ids-path arg

--- check_for_new_cases.py
+++ check_for_new_cases.py
@@ -12,6 +12,8 @@ p.add_argument("-m", "--metadata-table", action="append", help="optionally speci
 p.add_argument("-c", "--metadata-sample-id-column", default="sample_id", help="Sample id column in the metadata table")
 p.add_argument("-k", "--known-case-sample-ids-path", help="Path of a text file containing a list of sample ids (one "
                "per line) that are already known to have SMA")
+p.add_argument("-x", "--exclude-sample-ids-path", help="Path of a text file containing a list of sample ids (one per "
+               "line) to exclude from the results")
 p.add_argument("results_table", help="Path of the combined results table created by running SMA Finder on multiple "
                "samples and concatenating the output tables.")
 args = p.parse_args()
@@ -21,7 +23,7 @@ if not os.path.isfile(args.results_table):
     p.error(f"File not found: {args.results_table}")
 
 df = pd.read_table(args.results_table)
-df_columns = set(df.columns)
+df_standard_columns = set(df.columns)
 print(f"Parsed {len(df):,d} rows from {args.results_table}")
 
 def process_sample_id(s):
@@ -69,6 +71,21 @@ if args.known_case_sample_ids_path:
         }
     print(f"Loaded {len(known_cases):,d} known case sample ids from {args.known_case_sample_ids_path}")
 
+
+# load excluded sample ids
+exclude_sample_ids = set()
+if args.exclude_sample_ids_path:
+    if not os.path.exists(args.exclude_sample_ids_path):
+        p.error(f"File not found: {args.exclude_sample_ids_path}")
+
+    with open(args.exclude_sample_ids_path) as f:
+        exclude_sample_ids_path = {
+            process_sample_id(line.strip()) for line in f if line.strip()
+        }
+    print(f"Loaded {len(exclude_sample_ids_path):,d} known case sample ids from {args.exclude_sample_ids_path}")
+
+    df = df[~df.index.isin(exclude_sample_ids_path)]
+
 # print result stats
 sample_ids_not_has_sma = set(df[df.sma_status != 'has SMA'].index)
 sample_ids_has_sma = set(df[df.sma_status == 'has SMA'].index)
@@ -84,14 +101,29 @@ print(f"Overall, {len(sample_ids_has_sma):,d} samples have SMA in this callset: 
 df = df[df.sma_status == "has SMA"]
 df = df[~df.index.isin(known_cases)]
 
-
+# print stats on new cases
 if len(df) > 0:
     print(f"\nNew cases:")
+    extra_columns_to_print = set(df.columns) - df_standard_columns
+    extra_columns_to_print = {c for c in extra_columns_to_print if "path" not in c}
+    extra_columns_to_print = list(sorted(extra_columns_to_print))
+
+    column_widths = {c: len(c) for c in extra_columns_to_print}
     for i, (_, row) in enumerate(df.iterrows()):
-        print(f"#{i+1:<2d}  {row['filename_prefix']:20s}  {row['genome_version']}  "
-              f"{int(row['c840_reads_with_smn1_base_C']):5,d} out of {int(row['c840_total_reads']):5,d} reads")
-        for k, v in row.to_dict().items():
-            if k not in df_columns and not pd.isnull(v):
-                print(f"    {k}: {v}")
+        for c in extra_columns_to_print:
+            column_widths[c] = max(column_widths.get(c, 0), len(str(row.get(c, ""))))
+    header_line = f"{'#':<2s}  {'filename_prefix':20s}  {'genome_version':20s}  "
+    header_line += f"read support" + " "*15
+    for c in extra_columns_to_print:
+        header_line += f"{c:>{column_widths[c] + 5}s}"
+
+    print(header_line)
+    for i, (_, row) in enumerate(df.iterrows()):
+        print_line = f"#{i+1:<2d}  {row['filename_prefix']:20s}  {row['genome_version']:20s}  "
+        print_line += f"{int(row['c840_reads_with_smn1_base_C']):5,d} out of {int(row['c840_total_reads']):5,d} reads"
+        row_dict = row.to_dict()
+        for c in extra_columns_to_print:
+            print_line += f" {str(row_dict.get(c, '')):>{column_widths[c] + 5}s}"
+        print(print_line)
 else:
     print("No new cases")
